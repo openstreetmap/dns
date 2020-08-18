34ea885561bcbbbe130550bd9e4a8ab50fa6846d
Ian Dees <ian.dees@gmail.com>
2020-08-18 18:39:56 -0500

    Add proof of ownership for Fastly to openstreetmap.org

--- src/openstreetmap.js
+++ src/openstreetmap.js
@@ -28,6 +28,9 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
 
   TXT("_mta-sts", "v=STSv1; id=202001291805Z"),
   TXT("_smtp._tls", "v=TLSRPTv1; rua=mailto:postmaster@openstreetmap.org"),
+  
+  // Fastly cert domain ownership confirmation
+  TXT("_globalsign-domain-verification", "ps00GlW1BzY9c2_cwH_pFqRkvzZyaCVZ-3RLssRG6S"),
 
   // Delegate MTA-STS policy for subdomains
 
