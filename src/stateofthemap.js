c9c4c4b1bfd1de573fa222d6e8be473f79ad3b06
Tom Hughes <tom@compton.nu>
2021-02-22 19:12:41 +0000

    Add 2021.stateofthemap to DNS

--- src/stateofthemap.js
+++ src/stateofthemap.js
@@ -22,6 +22,7 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
 
   A("@", RIDLEY_IPV4, TTL("10m")),
   A("www", RIDLEY_IPV4, TTL("10m")),
+  A("2021", RIDLEY_IPV4, TTL("10m")),
   A("2020", RIDLEY_IPV4, TTL("10m")),
   A("2019", RIDLEY_IPV4, TTL("10m")),
   A("2018", RIDLEY_IPV4, TTL("10m")),
