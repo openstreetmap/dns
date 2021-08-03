c48eaf4dd52fba3d2ee6e0b1c99829b76e41c133
datendelphin <github@osm.datendelphin.net>
2021-08-03 21:10:33 +0200

    Update osm-li.js
    Hi,
    
    The SOSM servers got replaced by new ones, nalps and palpuogna.
    
    best regards
    Michael

--- src/osm-li.js
+++ src/osm-li.js
@@ -1,7 +1,7 @@
 D(DOMAIN, REGISTRAR,
 
-  NAMESERVER("falerin.sosm.ch."),
-  NAMESERVER("urlaun.sosm.ch."),
+  NAMESERVER("nalps.sosm.ch."),
+  NAMESERVER("palpuogna.sosm.ch."),
   NAMESERVER("ns.poole.ch."),
   NAMESERVER("he.poole.ch."),
   NAMESERVER("ns3.spreng.ch.")
