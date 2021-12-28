39373e5077a68861d27eb2eaf94e4c3115b768cb
Tom Hughes <tom@compton.nu>
2021-12-28 14:45:49 +0000

    Update IPv6 address for osm2pgsql.org

--- src/osm2pgsql.js
+++ src/osm2pgsql.js
@@ -17,8 +17,8 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
   // Main web server and it's aliases
 
   A("@", "138.201.190.130", TTL("10m")),
-  AAAA("@", "2a01:4f8:1c17:6433::1", TTL("10m")),
+  AAAA("@", "2a01:4f8:1c17:6433::2", TTL("10m")),
   A("www", "138.201.190.130", TTL("10m")),
-  AAAA("www", "2a01:4f8:1c17:6433::1", TTL("10m"))
+  AAAA("www", "2a01:4f8:1c17:6433::2", TTL("10m"))
 
 );
