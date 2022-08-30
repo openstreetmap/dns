f16c035f3c9f60b131892ac18a5f87a987a0f1b7
Grant Slater <git@firefishy.com>
2022-08-30 13:10:50 +0100

    Add test.osm2pgsql A record

--- src/osm2pgsql.js
+++ src/osm2pgsql.js
@@ -15,9 +15,12 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
 
   // Main web server and it's aliases
 
-  A("@", "138.201.190.130", TTL("10m")),
-  AAAA("@", "2a01:4f8:1c17:6433::2", TTL("10m")),
-  A("www", "138.201.190.130", TTL("10m")),
-  AAAA("www", "2a01:4f8:1c17:6433::2", TTL("10m"))
+  A("@", "138.201.190.130"),
+  AAAA("@", "2a01:4f8:1c17:6433::2"),
+  A("www", "138.201.190.130"),
+  AAAA("www", "2a01:4f8:1c17:6433::2"),
+
+  // Test server for osm2pgsql development
+  A("test", "135.181.221.216")
 
 );
