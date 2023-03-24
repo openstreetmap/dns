f1ffc9ed133ff9dcf6ded3927be9ecd771f6cf3a
Jonathan Beliën <jbelien@users.noreply.github.com>
2023-03-24 16:54:16 +0100

    Update stateofthemap-eu.js

--- src/stateofthemap-eu.js
+++ src/stateofthemap-eu.js
@@ -2,24 +2,26 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
 
   // Publish CAA records indicating that only letsencrypt should issue certificates
 
-  CAA_BUILDER({
-    label: "@",
-    iodef: "mailto:hostmaster@openstreetmap.org",
-    issue: [
-      "letsencrypt.org",
-    ],
-    issuewild: [
-      "letsencrypt.org",
-    ],
-  }),
+  // CAA_BUILDER({
+  //   label: "@",
+  //   iodef: "mailto:hostmaster@openstreetmap.org",
+  //   issue: [
+  //     "letsencrypt.org",
+  //   ],
+  //   issuewild: [
+  //     "letsencrypt.org",
+  //   ],
+  // }),
 
   // Let openstreetmap.at handle email
 
-  MX("@", 1, "mail.openstreetmap.at."),
+  // MX("@", 1, "mail.openstreetmap.at."),
 
   // Main web server and it's aliases
 
-  A("@", "88.198.206.107"),
-  A("www", "88.198.206.107")
+  A("@", "185.199.108.153"),
+  A("@", "185.199.109.153"),
+  A("@", "185.199.110.153"),
+  A("@", "185.199.111.153"),
 
 );
