2131839e59f0dee01080b1cb3c25f976f8bfd1e3
Kamil Monicz <kamil@monicz.dev>
2024-02-12 11:46:55 +0100

    Reconfigure DNS for the new edition

--- src/stateofthemap-eu.js
+++ src/stateofthemap-eu.js
@@ -13,25 +13,21 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
     ],
   }),
 
-  // Let OSMBE handle email (through Fastmail)
-  // https://www.fastmail.help/hc/en-us/articles/1500000280261
+  // Block email delivery
 
-  MX("@", 10, "in1-smtp.messagingengine.com."),
-  MX("@", 20, "in2-smtp.messagingengine.com."),
-  
-  TXT("@", "v=spf1 include:spf.messagingengine.com ?all"),
-  CNAME("fm1._domainkey", "fm1.stateofthemap.eu.dkim.fmhosted.com."),
-  CNAME("fm2._domainkey", "fm2.stateofthemap.eu.dkim.fmhosted.com."),
-  CNAME("fm3._domainkey", "fm3.stateofthemap.eu.dkim.fmhosted.com."),
+  TXT("_dmarc", "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"),
+  TXT("*._domainkey", "v=DKIM1; p="),
+  TXT("@", "v=spf1 -all"),
 
   // Site hosted on github pages
 
-  ALIAS("@", "osmbe.github.io."),
-  CNAME("www", "osmbe.github.io."),
+  ALIAS("@", "openstreetmap-polska.github.io."),
+  CNAME("www", "openstreetmap-polska.github.io."),
   
   // Previous editions
   
   A("2014", "49.12.5.171"),
-  CNAME("2023", "osmbe.github.io.")
+  CNAME("2023", "osmbe.github.io."),
+  CNAME("2024", "openstreetmap-polska.github.io.")
   
 );
