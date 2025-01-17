7478f25a912e2fc62cc88ff2702f75c059aeb492
Grant Slater <github@firefishy.com>
2025-01-17 22:59:10 +0000

    Use new cleaner PTR rfc4183 support

--- src/ptr_equinix_ams_ipv4.js
+++ src/ptr_equinix_ams_ipv4.js
@@ -1,15 +1,15 @@
 // http://www.he.net/adm/reverse.dns.html (RFC4183 notation)
 D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
-  PTR(DRIBBLE_IPV4.split(".").pop(), "dribble.openstreetmap.org."),
-  PTR(DULCY_IPV4.split(".").pop(), "dulcy.openstreetmap.org."),
-  PTR(FAFFY_IPV4.split(".").pop(), "faffy.openstreetmap.org."),
-  PTR(LOCKHEED_IPV4.split(".").pop(), "lockheed.openstreetmap.org."),
-  PTR(NORBERT_IPV4.split(".").pop(), "norbert.openstreetmap.org."),
-  PTR(ODIN_IPV4.split(".").pop(), "odin.openstreetmap.org."),
-  PTR(SPIKE06_IPV4.split(".").pop(), "spike-06.openstreetmap.org."),
-  PTR(SPIKE07_IPV4.split(".").pop(), "spike-07.openstreetmap.org."),
-  PTR(SPIKE08_IPV4.split(".").pop(), "spike-08.openstreetmap.org."),
-  PTR(TABALUGA_IPV4.split(".").pop(), "tabaluga.openstreetmap.org."),
-  PTR(VHAGAR_IPV4.split(".").pop(), "vhagar.openstreetmap.org."),
-  PTR(SWITCH1AMS_IPV4.split(".").pop(), "switch1.ams.openstreetmap.org.")
+  PTR(DRIBBLE_IPV4, "dribble.openstreetmap.org."),
+  PTR(DULCY_IPV4, "dulcy.openstreetmap.org."),
+  PTR(FAFFY_IPV4, "faffy.openstreetmap.org."),
+  PTR(LOCKHEED_IPV4, "lockheed.openstreetmap.org."),
+  PTR(NORBERT_IPV4, "norbert.openstreetmap.org."),
+  PTR(ODIN_IPV4, "odin.openstreetmap.org."),
+  PTR(SPIKE06_IPV4, "spike-06.openstreetmap.org."),
+  PTR(SPIKE07_IPV4, "spike-07.openstreetmap.org."),
+  PTR(SPIKE08_IPV4, "spike-08.openstreetmap.org."),
+  PTR(TABALUGA_IPV4, "tabaluga.openstreetmap.org."),
+  PTR(VHAGAR_IPV4, "vhagar.openstreetmap.org."),
+  PTR(SWITCH1AMS_IPV4, "switch1.ams.openstreetmap.org.")
 );
