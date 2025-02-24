d962baf34a5d0581f5d487c20f3a4efd21fa8121
Grant Slater <github@firefishy.com>
2025-02-24 21:51:45 +0000

    Use osm_web_service for openstreetmap.org

--- src/openstreetmap.js
+++ src/openstreetmap.js
@@ -148,76 +148,30 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
 
   // Main web servers and their aliases
 
-  HTTPS("www", 1, ".", "alpn=h2"),
-  HTTPS("api", 1, ".", "alpn=h2"),
-  HTTPS("maps", 1, ".", "alpn=h2"),
-  HTTPS("mapz", 1, ".", "alpn=h2"),
+  osm_web_service("@",    [ "spike-06",
+                            "spike-07",
+                            "spike-08"
+                          ], { cfproxy: true }),
 
-  // A("@", IPV4["spike-01.he"]),
-  // AAAA("@", IPV6["spike-01.he"]),
-  // A("www", IPV4["spike-01.he"]),
-  // AAAA("www", IPV6["spike-01.he"]),
-  // A("api", IPV4["spike-01.he"]),
-  // AAAA("api", IPV6["spike-01.he"]),
-  // A("maps", IPV4["spike-01.he"]),
-  // AAAA("maps", IPV6["spike-01.he"]),
-  // A("mapz", IPV4["spike-01.he"]),
-  // AAAA("mapz", IPV6["spike-01.he"]),
+  osm_web_service("www", [  "spike-06",
+                            "spike-07",
+                            "spike-08"
+                          ], { cfproxy: true }),
 
-  // A("@", IPV4["spike-02.he"]),
-  // AAAA("@", IPV6["spike-02.he"]),
-  // A("www", IPV4["spike-02.he"]),
-  // AAAA("www", IPV6["spike-02.he"]),
-  // A("api", IPV4["spike-02.he"]),
-  // AAAA("api", IPV6["spike-02.he"]),
-  // A("maps", IPV4["spike-02.he"]),
-  // AAAA("maps", IPV6["spike-02.he"]),
-  // A("mapz", IPV4["spike-02.he"]),
-  // AAAA("mapz", IPV6["spike-02.he"]),
+  osm_web_service("api", [  "spike-06",
+                            "spike-07",
+                            "spike-08"
+                          ], { cfproxy: true }),
 
-  // A("@", IPV4["spike-03.he"]),
-  // AAAA("@", IPV6["spike-03.he"]),
-  // A("www", IPV4["spike-03.he"]),
-  // AAAA("www", IPV6["spike-03.he"]),
-  // A("api", IPV4["spike-03.he"]),
-  // AAAA("api", IPV6["spike-03.he"]),
-  // A("maps", IPV4["spike-03.he"]),
-  // AAAA("maps", IPV6["spike-03.he"]),
-  // A("mapz", IPV4["spike-03.he"]),
-  // AAAA("mapz", IPV6["spike-03.he"]),
+  osm_web_service("maps", [ "spike-06",
+                            "spike-07",
+                            "spike-08"
+                          ], { cfproxy: true }),
 
-  A("@", IPV4["spike-06"], CF_PROXY_ON),
-  AAAA("@", IPV6["spike-06"], CF_PROXY_ON),
-  A("www", IPV4["spike-06"], CF_PROXY_ON),
-  AAAA("www", IPV6["spike-06"], CF_PROXY_ON),
-  A("api", IPV4["spike-06"], CF_PROXY_ON),
-  AAAA("api", IPV6["spike-06"], CF_PROXY_ON),
-  A("maps", IPV4["spike-06"], CF_PROXY_ON),
-  AAAA("maps", IPV6["spike-06"], CF_PROXY_ON),
-  A("mapz", IPV4["spike-06"], CF_PROXY_ON),
-  AAAA("mapz", IPV6["spike-06"], CF_PROXY_ON),
-
-  A("@", IPV4["spike-07"], CF_PROXY_ON),
-  AAAA("@", IPV6["spike-07"], CF_PROXY_ON),
-  A("www", IPV4["spike-07"], CF_PROXY_ON),
-  AAAA("www", IPV6["spike-07"], CF_PROXY_ON),
-  A("api", IPV4["spike-07"], CF_PROXY_ON),
-  AAAA("api", IPV6["spike-07"], CF_PROXY_ON),
-  A("maps", IPV4["spike-07"], CF_PROXY_ON),
-  AAAA("maps", IPV6["spike-07"], CF_PROXY_ON),
-  A("mapz", IPV4["spike-07"], CF_PROXY_ON),
-  AAAA("mapz", IPV6["spike-07"], CF_PROXY_ON),
-
-  A("@", IPV4["spike-08"], CF_PROXY_ON),
-  AAAA("@", IPV6["spike-08"], CF_PROXY_ON),
-  A("www", IPV4["spike-08"], CF_PROXY_ON),
-  AAAA("www", IPV6["spike-08"], CF_PROXY_ON),
-  A("api", IPV4["spike-08"], CF_PROXY_ON),
-  AAAA("api", IPV6["spike-08"], CF_PROXY_ON),
-  A("maps", IPV4["spike-08"], CF_PROXY_ON),
-  AAAA("maps", IPV6["spike-08"], CF_PROXY_ON),
-  A("mapz", IPV4["spike-08"], CF_PROXY_ON),
-  AAAA("mapz", IPV6["spike-08"], CF_PROXY_ON),
+  osm_web_service("mapz", [ "spike-06",
+                            "spike-07",
+                            "spike-08"
+                          ], { cfproxy: true }),
 
   // Nominatim servers
 
@@ -232,10 +186,9 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
   CNAME("b.tile", "dualstack.n.sni.global.fastly.net."),
   CNAME("c.tile", "dualstack.n.sni.global.fastly.net."),
 
-  A("render", IPV4["culebre.he"]),
-  A("render", IPV4["nidhogg"]),
-  AAAA("render", IPV6["culebre.he"]),
-  AAAA("render", IPV6["nidhogg"]),
+  osm_web_service("render", [ "culebre.he",
+                              "nidhogg"
+                            ]),
 
   // Vector tile servers
 
@@ -243,285 +196,136 @@ D(DOMAIN, REGISTRAR, DnsProvider(PROVIDER),
 
   // Planet servers
 
-  HTTPS("planet", 1, ".", "alpn=h2"),
-
   A("backup", IPV4["norbert"]),
   AAAA("backup", IPV6["norbert"]),
-  A("planet", IPV4["norbert"]),
-  AAAA("planet", IPV6["norbert"]),
-
   // A("backup", IPV4["horntail.he"]),
   // AAAA("backup", IPV6["horntail.he"]),
-  // A("planet", IPV4["horntail.he"]),
-  // AAAA("planet", IPV6["horntail.he"]),
+
+  osm_web_service("planet", "norbert"),
+  // osm_web_service("planet", "horntail.he"),
 
   // Development server with wildcard alias for user sites
 
-  A("dev", IPV4["faffy"]),
-  AAAA("dev", IPV6["faffy"]),
-  A("*.dev", IPV4["faffy"]),
-  AAAA("*.dev", IPV6["faffy"]),
-  A("ooc", IPV4["faffy"]),
-  AAAA("ooc", IPV6["faffy"]),
-  A("a.ooc", IPV4["faffy"]),
-  AAAA("a.ooc", IPV6["faffy"]),
-  A("b.ooc", IPV4["faffy"]),
-  AAAA("b.ooc", IPV6["faffy"]),
-  A("c.ooc", IPV4["faffy"]),
-  AAAA("c.ooc", IPV6["faffy"]),
-  A("npe", IPV4["faffy"]),
-  AAAA("npe", IPV6["faffy"]),
+  osm_web_service("dev",   "faffy"),
+  osm_web_service("*.dev", "faffy"),
+  osm_web_service("ooc",   "faffy"),
+  osm_web_service("a.ooc", "faffy"),
+  osm_web_service("b.ooc", "faffy"),
+  osm_web_service("c.ooc", "faffy"),
+  osm_web_service("npe",   "faffy"),
 
   // Foundation server
 
-  HTTPS("blog", 1, ".", "alpn=h2"),
-  HTTPS("foundation", 1, ".", "alpn=h2"),
-
-  A("blog", IPV4["ridley"]),
-  A("foundation", IPV4["ridley"]),
+  osm_web_service("blog",       "ridley"),
+  osm_web_service("foundation", "ridley"),
 
   // Matomo server
 
-  HTTPS("matomo", 1, ".", "alpn=h2"),
-  HTTPS("piwik", 1, ".", "alpn=h2"),
-
-  A("matomo", IPV4["smaug.he"]),
-  AAAA("matomo", IPV6["smaug.he"]),
-  A("piwik", IPV4["smaug.he"]),
-  AAAA("piwik", IPV6["smaug.he"]),
+  osm_web_service("matomo", "smaug.he"),
+  osm_web_service("piwik",  "smaug.he"),
 
   // Imagery servers
 
-  HTTPS("agri", 1, ".", "alpn=h2"),
-  HTTPS("a.agri", 1, ".", "alpn=h2"),
-  HTTPS("b.agri", 1, ".", "alpn=h2"),
-  HTTPS("c.agri", 1, ".", "alpn=h2"),
+  osm_web_service("agri",   "lockheed"),
+  osm_web_service("a.agri", "lockheed"),
+  osm_web_service("b.agri", "lockheed"),
+  osm_web_service("c.agri", "lockheed"),
 
-  A("agri", IPV4["lockheed"]),
-  AAAA("agri", IPV6["lockheed"]),
-  A("a.agri", IPV4["lockheed"]),
-  AAAA("a.agri", IPV6["lockheed"]),
-  A("b.agri", IPV4["lockheed"]),
-  AAAA("b.agri", IPV6["lockheed"]),
-  A("c.agri", IPV4["lockheed"]),
-  AAAA("c.agri", IPV6["lockheed"]),
+  osm_web_service("act-imagery",   "lockheed"),
+  osm_web_service("a.act-imagery", "lockheed"),
+  osm_web_service("b.act-imagery", "lockheed"),
+  osm_web_service("c.act-imagery", "lockheed"),
 
-  HTTPS("act-imagery", 1, ".", "alpn=h2"),
-  HTTPS("a.act-imagery", 1, ".", "alpn=h2"),
-  HTTPS("b.act-imagery", 1, ".", "alpn=h2"),
-  HTTPS("c.act-imagery", 1, ".", "alpn=h2"),
+  osm_web_service("au-vic-melbourne-imagery",   "lockheed"),
+  osm_web_service("a.au-vic-melbourne-imagery", "lockheed"),
+  osm_web_service("b.au-vic-melbourne-imagery", "lockheed"),
+  osm_web_service("c.au-vic-melbourne-imagery", "lockheed"),
 
-  A("act-imagery", IPV4["lockheed"]),
-  AAAA("act-imagery", IPV6["lockheed"]),
-  A("a.act-imagery", IPV4["lockheed"]),
-  AAAA("a.act-imagery", IPV6["lockheed"]),
-  A("b.act-imagery", IPV4["lockheed"]),
-  AAAA("b.act-imagery", IPV6["lockheed"]),
-  A("c.act-imagery", IPV4["lockheed"]),
-  AAAA("c.act-imagery", IPV6["lockheed"]),
+  osm_web_service("os",   "lockheed"),
+  osm_web_service("a.os", "lockheed"),
+  osm_web_service("b.os", "lockheed"),
+  osm_web_service("c.os", "lockheed"),
 
-  HTTPS("au-vic-melbourne-imagery", 1, ".", "alpn=h2"),
-  HTTPS("a.au-vic-melbourne-imagery", 1, ".", "alpn=h2"),
-  HTTPS("b.au-vic-melbourne-imagery", 1, ".", "alpn=h2"),
-  HTTPS("c.au-vic-melbourne-imagery", 1, ".", "alpn=h2"),
+  osm_web_service("tiler", "lockheed"),
 
-  A("au-vic-melbourne-imagery", IPV4["lockheed"]),
-  AAAA("au-vic-melbourne-imagery", IPV6["lockheed"]),
-  A("a.au-vic-melbourne-imagery", IPV4["lockheed"]),
-  AAAA("a.au-vic-melbourne-imagery", IPV6["lockheed"]),
-  A("b.au-vic-melbourne-imagery", IPV4["lockheed"]),
-  AAAA("b.au-vic-melbourne-imagery", IPV6["lockheed"]),
-  A("c.au-vic-melbourne-imagery", IPV4["lockheed"]),
-  AAAA("c.au-vic-melbourne-imagery", IPV6["lockheed"]),
-
-  HTTPS("os", 1, ".", "alpn=h2"),
-  HTTPS("a.os", 1, ".", "alpn=h2"),
-  HTTPS("b.os", 1, ".", "alpn=h2"),
-  HTTPS("c.os", 1, ".", "alpn=h2"),
-
-  A("os", IPV4["lockheed"]),
-  AAAA("os", IPV6["lockheed"]),
-  A("a.os", IPV4["lockheed"]),
-  AAAA("a.os", IPV6["lockheed"]),
-  A("b.os", IPV4["lockheed"]),
-  AAAA("b.os", IPV6["lockheed"]),
-  A("c.os", IPV4["lockheed"]),
-  AAAA("c.os", IPV6["lockheed"]),
-
-  HTTPS("tiler", 1, ".", "alpn=h2"),
-  HTTPS("us-imagery", 1, ".", "alpn=h2"),
-  HTTPS("a.us-imagery", 1, ".", "alpn=h2"),
-  HTTPS("b.us-imagery", 1, ".", "alpn=h2"),
-  HTTPS("c.us-imagery", 1, ".", "alpn=h2"),
-
-  A("tiler", IPV4["lockheed"]),
-  AAAA("tiler", IPV6["lockheed"]),
-  A("us-imagery", IPV4["lockheed"]),
-  AAAA("us-imagery", IPV6["lockheed"]),
-  A("a.us-imagery", IPV4["lockheed"]),
-  AAAA("a.us-imagery", IPV6["lockheed"]),
-  A("b.us-imagery", IPV4["lockheed"]),
-  AAAA("b.us-imagery", IPV6["lockheed"]),
-  A("c.us-imagery", IPV4["lockheed"]),
-  AAAA("c.us-imagery", IPV6["lockheed"]),
+  osm_web_service("us-imagery",   "lockheed"),
+  osm_web_service("a.us-imagery", "lockheed"),
+  osm_web_service("b.us-imagery", "lockheed"),
+  osm_web_service("c.us-imagery", "lockheed"),
 
   // Prometheus server and munin redirect
 
-  HTTPS("prometheus", 1, ".", "alpn=h2"),
-  HTTPS("munin", 1, ".", "alpn=h2"),
-
-  A("prometheus", IPV4["stormfly-03"]),
-  AAAA("prometheus", IPV6["stormfly-03"]),
-  A("munin", IPV4["stormfly-03"]),
-  AAAA("munin", IPV6["stormfly-03"]),
+  osm_web_service("prometheus", "stormfly-03"),
+  osm_web_service("munin",      "stormfly-03"),
 
   // Management server
 
-  HTTPS("acme", 1, ".", "alpn=h2"),
-  HTTPS("chef", 1, ".", "alpn=h2"),
-  HTTPS("dns", 1, ".", "alpn=h2"),
-  HTTPS("git", 1, ".", "alpn=h2"),
-  HTTPS("hardware", 1, ".", "alpn=h2"),
-
-  A("acme", IPV4["idris.he"]),
-  AAAA("acme", IPV6["idris.he"]),
-  A("apt", IPV4["idris.he"]),
-  AAAA("apt", IPV6["idris.he"]),
-  A("chef", IPV4["idris.he"]),
-  AAAA("chef", IPV6["idris.he"]),
-  A("dns", IPV4["idris.he"]),
-  AAAA("dns", IPV6["idris.he"]),
-  A("git", IPV4["idris.he"]),
-  AAAA("git", IPV6["idris.he"]),
-  A("hardware", IPV4["idris.he"]),
-  AAAA("hardware", IPV6["idris.he"]),
+  osm_web_service("acme",     "idris.he"),
+  osm_web_service("apt",      "idris.he"),
+  osm_web_service("chef",     "idris.he"),
+  osm_web_service("dns",      "idris.he"),
+  osm_web_service("git",      "idris.he"),
+  osm_web_service("hardware", "idris.he"),
 
   // Bytemark machine, and the services which operate from it
 
-  HTTPS("lists", 1, ".", "alpn=h2"),
-  HTTPS("help", 1, ".", "alpn=h2"),
-
-  A("lists", IPV4["shenron"]),
-  AAAA("lists", IPV6["shenron"]),
-  A("help", IPV4["shenron"]),
-  AAAA("help", IPV6["shenron"]),
+  osm_web_service("lists", "shenron"),
+  osm_web_service("help",  "shenron"),
 
   // Naga services
 
-  HTTPS("svn", 1, ".", "alpn=h2"),
-  HTTPS("trac", 1, ".", "alpn=h2"),
-  HTTPS("irc", 1, ".", "alpn=h2"),
-  HTTPS("blogs", 1, ".", "alpn=h2"),
-  HTTPS("welcome", 1, ".", "alpn=h2"),
-  HTTPS("operations", 1, ".", "alpn=h2"),
-  HTTPS("hot", 1, ".", "alpn=h2"),
-  HTTPS("dmca", 1, ".", "alpn=h2"),
-  // HTTPS("otrs", 1, ".", "alpn=h2"), - OTRS is not available using HTTPS/2
-  HTTPS("birthday20", 1, ".", "alpn=h2"),
-
-  A("svn", IPV4["naga.he"]),
-  AAAA("svn", IPV6["naga.he"]),
-  A("trac", IPV4["naga.he"]),
-  AAAA("trac", IPV6["naga.he"]),
-  A("irc", IPV4["naga.he"]),
-  AAAA("irc", IPV6["naga.he"]),
-  A("blogs", IPV4["naga.he"]),
-  AAAA("blogs", IPV6["naga.he"]),
-  A("welcome", IPV4["naga.he"]),
-  AAAA("welcome", IPV6["naga.he"]),
-  A("operations", IPV4["naga.he"]),
-  AAAA("operations", IPV6["naga.he"]),
-  A("hot", IPV4["naga.he"]),
-  AAAA("hot", IPV6["naga.he"]),
-  A("dmca", IPV4["naga.he"]),
-  AAAA("dmca", IPV6["naga.he"]),
-  A("otrs", IPV4["naga.he"]),
-  AAAA("otrs", IPV6["naga.he"]),
-  A("birthday20", IPV4["naga.he"]),
-  AAAA("birthday20", IPV6["naga.he"]),
+  osm_web_service("svn",        "naga.he"),
+  osm_web_service("trac",       "naga.he"),
+  osm_web_service("irc",        "naga.he"),
+  osm_web_service("blogs",      "naga.he"),
+  osm_web_service("welcome",    "naga.he"),
+  osm_web_service("operations", "naga.he"),
+  osm_web_service("hot",        "naga.he"),
+  osm_web_service("dmca",       "naga.he"),
+  osm_web_service("otrs",       "naga.he", { h1: true, h2: false }), // OTRS is not available using HTTPS/2
+  osm_web_service("birthday20", "naga.he"),
 
   // Wiki servers
 
-  HTTPS("wiki", 1, ".", "alpn=h2"),
-
-  A("wiki", IPV4["konqi.he"]),
-  AAAA("wiki", IPV6["konqi.he"]),
+  osm_web_service("wiki", "konqi.he"),
 
   // Overpass server
 
-  HTTPS("query", 1, ".", "alpn=h2"),
-
-  A("query", IPV4["grisu.he"]),
-  AAAA("query", IPV6["grisu.he"]),
+  osm_web_service("query", "grisu.he"),
 
   // GPS tile server
 
-  HTTPS("gps-tile", 1, ".", "alpn=h2"),
-  HTTPS("a.gps-tile", 1, ".", "alpn=h2"),
-  HTTPS("b.gps-tile", 1, ".", "alpn=h2"),
-  HTTPS("c.gps-tile", 1, ".", "alpn=h2"),
-  HTTPS("gps-a.tile", 1, ".", "alpn=h2"),
-  HTTPS("gps-b.tile", 1, ".", "alpn=h2"),
-  HTTPS("gps-c.tile", 1, ".", "alpn=h2"),
-
-  A("gps-tile", IPV4["muirdris.he"]),
-  AAAA("gps-tile", IPV6["muirdris.he"]),
-  A("a.gps-tile", IPV4["muirdris.he"]),
-  AAAA("a.gps-tile", IPV6["muirdris.he"]),
-  A("b.gps-tile", IPV4["muirdris.he"]),
-  AAAA("b.gps-tile", IPV6["muirdris.he"]),
-  A("c.gps-tile", IPV4["muirdris.he"]),
-  AAAA("c.gps-tile", IPV6["muirdris.he"]),
-  A("gps.tile", IPV4["muirdris.he"]),
-  AAAA("gps.tile", IPV6["muirdris.he"]),
-  A("gps-a.tile", IPV4["muirdris.he"]),
-  AAAA("gps-a.tile", IPV6["muirdris.he"]),
-  A("gps-b.tile", IPV4["muirdris.he"]),
-  AAAA("gps-b.tile", IPV6["muirdris.he"]),
-  A("gps-c.tile", IPV4["muirdris.he"]),
-  AAAA("gps-c.tile", IPV6["muirdris.he"]),
+  osm_web_service("gps-tile",   "muirdris.he"),
+  osm_web_service("a.gps-tile", "muirdris.he"),
+  osm_web_service("b.gps-tile", "muirdris.he"),
+  osm_web_service("c.gps-tile", "muirdris.he"),
+  osm_web_service("gps.tile",   "muirdris.he"),
+  osm_web_service("gps-a.tile", "muirdris.he"),
+  osm_web_service("gps-b.tile", "muirdris.he"),
+  osm_web_service("gps-c.tile", "muirdris.he"),
 
   // Donation site and new OSMF crm site
 
-  HTTPS("donate", 1, ".", "alpn=h2"),
-  HTTPS("support", 1, ".", "alpn=h2"),
-  HTTPS("supporting", 1, ".", "alpn=h2"),
-
-  A("donate", IPV4["ridley"]),
-  A("support", IPV4["ridley"]),
-  A("supporting", IPV4["ridley"]),
+  osm_web_service("donate",     "ridley"),
+  osm_web_service("support",    "ridley"),
+  osm_web_service("supporting", "ridley"),
 
   // Discourse server ("community")
 
-  HTTPS("community", 1, ".", "alpn=h2"),
-  HTTPS("communities", 1, ".", "alpn=h2"),
-  HTTPS("c", 1, ".", "alpn=h2"),
-
-  A("community", IPV4["fume.he"]),
-  A("communities", IPV4["fume.he"]),
-  A("c", IPV4["fume.he"]),
-  AAAA("community", IPV6["fume.he"]),
-  AAAA("communities", IPV6["fume.he"]),
-  AAAA("c", IPV6["fume.he"]),
+  osm_web_service("community",   "fume.he"),
+  osm_web_service("communities", "fume.he"),
+  osm_web_service("c",           "fume.he"),
+  osm_web_service("forum",       "fume.he"),
 
   CNAME("community-cdn", "dualstack.n.sni.global.fastly.net."),
   TXT("community", "google-site-verification=hQ8GZyj4KwnPqAX2oAzpbLrh6I5dfR08PSdL3icVkfg"),
 
-  HTTPS("forum", 1, ".", "alpn=h2"),
-
-  A("forum", IPV4["fume.he"]),
-  AAAA("forum", IPV6["fume.he"]),
-
   // Taginfo and Staging Blog Server
 
-  HTTPS("taginfo", 1, ".", "alpn=h2"),
+  osm_web_service("taginfo", "tabaluga"),
 
-  A("taginfo", IPV4["tabaluga"]),
-  AAAA("taginfo", IPV6["tabaluga"]),
+  // Staging Blog Server
 
-  HTTPS("staging.blog", 1, ".", "alpn=h2"),
-
-  A("staging.blog", IPV4["tabaluga"]),
-  AAAA("staging.blog", IPV6["tabaluga"])
+  osm_web_service("staging.blog", "tabaluga"),
 
 );
